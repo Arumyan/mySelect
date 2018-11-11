143d402884162984fb7054129ed0501fcdb61dc8
Arumyan <alekseyrumyancev@mail.ru>
2018-11-11 19:52:39 +0300

    my version select

--- customSelect.js
+++ customSelect.js
@@ -1,45 +1,50 @@
-function CustomSelect(elem) {
+function CustomSelect(elem, selectedField) {
 
-    this.elem = elem;
-    this.isOpen = false;
-    var myselect = this;
+	this.elem = document.querySelector(elem);
+	this.selectedField = document.querySelector(selectedField);
+	this.isOpen = false;
+	var myselect = this;
 
-    this.toggle = function() {
-      if (this.isOpen)
-        myselect.close()
-      else myselect.open();
-    };
+	// функция toggle для select
+	this.toggle = function () {
+		if (this.isOpen)
+			myselect.close()
+		else myselect.open();
+	};
 
-    this.open = function() {
-      this.elem.classList.add('open');
-      document.addEventListener('click', myselect.onDocumentClick);
-      this.isOpen = true;
-    }
+	// функция открытия select
+	this.open = function () {
+		this.elem.classList.add('open');
+		document.addEventListener('click', myselect.onDocumentClick);
+		this.isOpen = true;
+	}
 
-    this.close = function() {
-      myselect.elem.classList.remove('open');
-      document.removeEventListener('click', myselect.onDocumentClick);
-      this.isOpen = false;
-    }
+	// функция закрытия select
+	this.close = function () {
+		myselect.elem.classList.remove('open');
+		document.removeEventListener('click', myselect.onDocumentClick);
+		this.isOpen = false;
+	}
 
-    // закрыть селект, если клик вне его
-    this.onDocumentClick = function(event) {
-      if (!myselect.elem.contains(event.target))
-        myselect.close();
-    }
+	// закрыть селект, если клик вне его
+	this.onDocumentClick = function (event) {
+		if (!myselect.elem.contains(event.target))
+			myselect.close();
+	}
 
+	// обработка события при клике на select
+	this.elem.addEventListener('click', function (event) {
+		if (event.target == myselect.selectedField) {
+			myselect.toggle();
+		} else if (event.target.tagName == 'LI') {
+			myselect.setValue(event.target.innerHTML, event.target.dataset.value);
+			myselect.close();
+		}
+	})
 
-    this.elem.addEventListener('click', function (event) {
-      if (event.target.className == 'title') {
-        myselect.toggle();
-      } else if (event.target.tagName == 'LI') {
-        myselect.setValue(event.target.innerHTML, event.target.dataset.value);
-        myselect.close();
-      }
-    })
-
-    this.setValue = function(title, value) {
-      this.elem.querySelector('.title').innerHTML = title;
-      this.elem.querySelector('.title').setAttribute('data-val', value);
-    }
-  }
+	// установка выбранного поля и значения атрибута
+	this.setValue = function (title, value) {
+		this.selectedField.innerHTML = title;
+		this.selectedField.setAttribute('data-val', value);
+	}
+}
