e0018c92e8dccf88ca36a0140131230beb2292b8
Arumyan <alekseyrumyancev@mail.ru>
2018-11-11 19:13:23 +0300

    change code

--- customSelect.js
+++ customSelect.js
@@ -1,29 +1,56 @@
-function CustomSelect(options) {
-    var elem = options.elem;
-  
-    elem.onclick = function(event) {
-      if (event.target.className == 'title') {
-        toggle();
-      } else if (event.target.tagName == 'LI') {
-        setValue(event.target.innerHTML, event.target.dataset.value);
-        close();
-      }
+function CustomSelect(elem) {
+
+    this.elem = elem;
+    this.isOpen = false;
+    var myselect = this;
+
+    this.toggle = function() {
+      if (this.isOpen) myselect.close()
+        else myselect.open();
+    };
+
+    this.open = function() {
+      this.elem.classList.add('open');
+      document.addEventListener('click', myselect.onDocumentClick);
+      this.isOpen = true;
     }
-  
-    var isOpen = false;
-  
-    // ------ обработчики ------
-  
+
+    this.close = function() {
+      myselect.elem.classList.remove('open');
+      document.removeEventListener('click', myselect.onDocumentClick);
+      this.isOpen = false;
+    }
+
     // закрыть селект, если клик вне его
-    function onDocumentClick(event) {
-      if (!elem.contains(event.target)) close();
+    this.onDocumentClick = function(event) {
+      if (!myselect.elem.contains(event.target)) myselect.close();
     }
-  
+
+
+    this.elem.addEventListener('click', function (event) {
+      if (event.target.className == 'title') {
+        myselect.toggle();
+      } else if (event.target.tagName == 'LI') {
+        myselect.setValue(event.target.innerHTML, event.target.dataset.value);
+        myselect.close();
+      }
+    })
+
+    this.setValue = function(title, value) {
+      this.elem.querySelector('.title').innerHTML = title;
+    }
+  }
+
+    // закрыть селект, если клик вне его
+    /* function onDocumentClick(event) {
+      if (!this.elem.contains(event.target)) myselect.close();
+    } */
+
     // ------------------------
-  
-    function setValue(title, value) {
-      elem.querySelector('.title').innerHTML = title;
-  
+
+    /* function setValue(title, value) {
+      this.elem.querySelector('.title').innerHTML = title;
+
       var widgetEvent = new CustomEvent('select', {
         bubbles: true,
         detail: {
@@ -31,26 +58,7 @@ function CustomSelect(options) {
           value: value
         }
       });
-  
-      elem.dispatchEvent(widgetEvent);
-  
-    }
-  
-    function toggle() {
-      if (isOpen) close()
-      else open();
-    }
-  
-    function open() {
-      elem.classList.add('open');
-      document.addEventListener('click', onDocumentClick);
-      isOpen = true;
-    }
-  
-    function close() {
-      elem.classList.remove('open');
-      document.removeEventListener('click', onDocumentClick);
-      isOpen = false;
-    }
-  
-  }
+
+      myselect.elem.dispatchEvent(widgetEvent);
+
+    } */
